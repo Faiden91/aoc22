782252a085621a4c0b2055856f423603e250d216
Faiden91 <60633245+Faiden91@users.noreply.github.com>
2023-01-15 19:57:35 -0800

    Day1
    Day1 Answers

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 
 namespace Day1
 {
@@ -6,7 +8,106 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = @"
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+";
+
+
+            // check to see if this is a blank line or not
+            bool IsBlankLine(string line) => string.IsNullOrWhiteSpace(line);
+
+            //var lines = input.Split(new[] { '\r'}, StringSplitOptions.RemoveEmptyEntries);
+
+            var lines = File.ReadAllLines(@"C:\Users\Black\Documents\GitHub\aoc22\input.txt");
+
+            var largestLine = 0;
+            var largestTemp = 0;
+            var topthree = new int[3];
+
+            foreach (var line in lines)
+            {
+                if (IsBlankLine(line))
+                {
+                    if (largestTemp > topthree[0])
+                    {
+                        topthree[2] = topthree[1];
+                        topthree[1] = topthree[0];
+                        topthree[0] = largestTemp;
+                    }
+                    else if (largestTemp > topthree[1])
+                    {
+                        topthree[2] = topthree[1];
+                        topthree[1] = largestTemp;
+                    }
+                    else if (largestTemp > topthree[2])
+                    {
+                        topthree[2] = largestTemp;
+                    }
+
+                    largestTemp = 0;
+                    continue;
+                }
+
+                largestTemp += int.Parse(line);
+            }
+
+            foreach (var line in topthree)
+            {
+                largestLine += line;
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine(largestLine);
+
+
+
+
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for (int i = 0; i < text.Length; i++)
+//{
+//    if (Int32.TryParse(text[i], out number))
+//    {
+//        tempNumber += number;
+//        if (tempNumber > largestCurrentValue)
+//        {
+//            largestCurrentValue = tempNumber;
+//        }
+//    }
+//}
